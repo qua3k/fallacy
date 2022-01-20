d7391a8ee0ea262b68fd0048d73372238c0d0e0d
qua3k <cliffmaceyak@gmail.com>
2022-01-19 19:04:19 -0500

    Refactor commands.go
    *   Add BanServer function
    *   Fix MuteUser logic
    *   Additional nullptr checks in purgemessages

--- commands.go
+++ commands.go
@@ -28,15 +28,7 @@ func (f *Fallacy) acls(roomID id.RoomID) (resp event.ServerACLEventContent, err 
 	return
 }
 
-// isAdmin returns whether the user is a room admin by checking ban/kick/redact
-// power levels.
-func (f *Fallacy) isAdmin(roomID id.RoomID, userID id.UserID) bool {
-	pl, err := f.powerLevels(roomID)
-	if err != nil {
-		log.Println("fetching power levels event failed!")
-		return false
-	}
-
+func isAdmin(pl event.PowerLevelsEventContent, roomID id.RoomID, userID id.UserID) bool {
 	bp, kp, rp := pl.Ban(), pl.Kick(), pl.Redact()
 
 	minInt := func(i ...int) (m int) {
@@ -52,16 +44,54 @@ func (f *Fallacy) isAdmin(roomID id.RoomID, userID id.UserID) bool {
 	return pl.GetUserLevel(userID) >= minInt(bp, kp, rp)
 }
 
+// isAdmin returns whether the user is a room admin by checking ban/kick/redact
+// power levels.
+func (f *Fallacy) isAdmin(roomID id.RoomID, userID id.UserID) bool {
+	pl, err := f.powerLevels(roomID)
+	if err != nil {
+		log.Println("fetching power levels event failed!")
+		return false
+	}
+
+	return isAdmin(pl, roomID, userID)
+}
+
+// BanServer bans a server by adding it to the room ACL.
+func (f *Fallacy) BanServer(roomID id.RoomID, homeserverID string) (err error) {
+	acls, err := f.acls(roomID)
+	if err != nil {
+		return
+	}
+
+	for _, server := range acls.Allow {
+		if server == homeserverID {
+			return
+		}
+	}
+
+	for _, server := range acls.Deny {
+		if server == homeserverID {
+			return
+		}
+	}
+
+	acls.Deny = append(acls.Deny, homeserverID)
+	_, err = f.Client.SendStateEvent(roomID, event.StateServerACL, "", &acls)
+	return
+}
+
 // MuteUser mutes a target user in a specified room by utilizing power levels.
 func (f *Fallacy) MuteUser(roomID id.RoomID, targetID id.UserID) (err error) {
 	pl, err := f.powerLevels(roomID)
 	if err != nil {
 		return
 	}
-	level := pl.GetEventLevel(event.EventMessage)
-	if pl.GetUserLevel(targetID) <= level-1 {
+
+	level := pl.GetEventLevel(event.EventMessage) - 1
+	if pl.GetUserLevel(targetID) <= level {
 		return errors.New("cannot mute a user that is already muted")
 	}
+
 	pl.SetUserLevel(targetID, level)
 	_, err = f.Client.SendStateEvent(roomID, event.StatePowerLevels, "", &pl)
 	return
@@ -73,36 +103,45 @@ func (f *Fallacy) UnmuteUser(roomID id.RoomID, targetID id.UserID) (err error) {
 	if err != nil {
 		return
 	}
+
 	level := pl.GetEventLevel(event.EventMessage)
 	if pl.GetUserLevel(targetID) >= level {
 		return errors.New("cannot unmute a user that is not muted")
 	}
+
 	pl.SetUserLevel(targetID, level)
 	_, err = f.Client.SendStateEvent(roomID, event.StatePowerLevels, "", &pl)
 	return
 }
 
 // RedactMessage only redacts message events, skipping redaction events, already
-// redacted events, and state events. Rather than checking for the presence of
-// keys in the content object, we check for the presence of the redacted_because
-// object to ensure future compatibility with e2ee.
-func (f *Fallacy) RedactMessage(ev *event.Event, wait *sync.WaitGroup) {
-	defer wait.Done()
-	if ev.StateKey == nil ||
-		ev.Type != event.EventRedaction ||
-		ev.Unsigned.RedactedBecause == nil {
-		if _, err := f.Client.RedactEvent(ev.RoomID, ev.ID, mautrix.ReqRedact{}); err != nil {
-			log.Println("redacting message failed with error:", err)
-		}
+// redacted events, and state events.
+func (f *Fallacy) RedactMessage(ev event.Event) (err error) {
+	switch {
+	case ev.StateKey == nil,
+		ev.Type != event.EventRedaction,
+		ev.Unsigned.RedactedBecause == nil:
+		_, err = f.Client.RedactEvent(ev.RoomID, ev.ID, mautrix.ReqRedact{})
 	}
+	return
+}
+
+func parseMessage(ev event.Event) (id.EventID, id.RoomID, id.UserID) {
+	return ev.ID, ev.RoomID, ev.Sender
 }
 
 // PurgeMessages redacts all message events newer than the specified event ID.
 // It's loosely inspired by Telegram's SophieBot mechanics.
-//
-// TODO: check for races
-func (f *Fallacy) PurgeMessages(roomID id.RoomID, eventID id.EventID) error {
+func (f *Fallacy) PurgeMessages(ev event.Event) (err error) {
 	var wg sync.WaitGroup
+	const maxFetchLimit = 2147483647 // the maximum number of messages to fetch.
+
+	redactWait := func(ev event.Event) {
+		defer wg.Done()
+		if err := f.RedactMessage(ev); err != nil {
+			log.Println("redacting message failed with error:", err)
+		}
+	}
 
 	purgeMessages := func(s []*event.Event) {
 		for _, e := range s {
@@ -110,26 +149,30 @@ func (f *Fallacy) PurgeMessages(roomID id.RoomID, eventID id.EventID) error {
 				continue
 			}
 			wg.Add(1)
-			go f.RedactMessage(e, &wg)
+			go redactWait(*e) // check for races
 		}
 	}
 
 	filter := setupPurgeFilter()
+	eventID, roomID, _ := parseMessage(ev)
+
 	con, err := f.Client.Context(roomID, eventID, &filter, 1)
 	if err != nil {
 		return err
 	}
 
-	wg.Add(1)
-	go f.RedactMessage(con.Event, &wg)
+	if con.Event != nil {
+		wg.Add(1)
+		go redactWait(*con.Event)
+	}
 	purgeMessages(con.EventsAfter)
 
-	msg, err := f.Client.Messages(roomID, con.End, "", 'f', &filter, 2147483647) // will the server give us all these events?
+	msg, err := f.Client.Messages(roomID, con.End, "", 'f', &filter, maxFetchLimit)
 	if err != nil {
 		return err
 	}
 	purgeMessages(msg.Chunk)
 
 	wg.Wait()
-	return nil
+	return
 }
